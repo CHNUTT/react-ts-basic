adff5f6ac62dfefdd89d52590f962e3aeca44a02
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-05-04 17:13:38 -0500

    [ Component - Type ] - Explicit define component type annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -5,3 +5,15 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>;
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
+
+export const ChildAsFullFC: React.FunctionComponent<ChildProps> = ({
+  color,
+}) => {
+  return <div>{color}</div>;
+};
+
+ChildAsFC.displayName = '';
